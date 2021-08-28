f1b69216cf31a15641cb34812fbcc25e6ae1f817
ArtInLines <linesinart@gmail.com>
2021-08-28 23:07:57 +0200

    Added more api routes

--- server/routes/data.js
+++ server/routes/data.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Models = require('./../models/all');
 const { sendError, sendSuccess } = require('./helper');
 const { capitalize } = require('./../util/str');
+const { Model } = require('mongoose'); // For documentation, might remove later
 
 /**
  * Gets the category from a request object or the category string itself and capitalizes the string
@@ -20,6 +21,13 @@ const getURLParams = (params) => {
 	return params;
 };
 
+/** @returns {Model} */
+const getModel = (category) => {
+	if (Models.hasOwnProperty(category)) return Models[category].model;
+	sendError(res, `Category "${category}" doesn't exist`, 400);
+	return undefined;
+};
+
 const defaultCB = (res, err, docs) => {
 	if (err) sendError(res, err);
 	else sendSuccess(res, docs);
@@ -28,15 +36,39 @@ const defaultCB = (res, err, docs) => {
 router
 	.get('/:category/:key=:val', async (req, res) => {
 		const { category, key, val } = getURLParams(req.params);
-		Models[category].model.find({ [key]: val }, (...args) => defaultCB(res, ...args));
+		const model = getModel(category);
+		if (model === undefined) return;
+		model.findOne({ [key]: val }, (...args) => defaultCB(res, ...args));
+	})
+	.get('/category/:key=:val/all', async (req, res) => {
+		const { category, key, val } = getURLParams(req.params);
+		const model = getModel(category);
+		if (model === undefined) return;
+		model.find({ [key]: val }, (...args) => defaultCB(res, ...args));
+	})
+	.get('/:category/all', async (req, res) => {
+		const model = getModel(getCategory(req));
+		if (model === undefined) return;
+		model.find((...args) => defaultCB(res, ...args));
 	})
 	.get('/:category/:id', async (req, res) => {
 		const { category, id } = getURLParams(req.params);
-		Models[category].model.findById(id, (...args) => defaultCB(res, ...args));
+		const model = getModel(category);
+		if (model === undefined) return;
+		model.findById(id, (...args) => defaultCB(res, ...args));
 	})
 	.post('/:category', async (req, res) => {
-		const { category } = getURLParams(req.params);
-		Models[category].model.create(req.body.data, {}, (...args) => defaultCB(res, ...args));
-	});
+		const model = getModel(getCategory(req));
+		if (model === undefined) return;
+		model.create(req.body.data, {}, (...args) => defaultCB(res, ...args));
+	})
+	.put('/:category/:key=:val', async (req, res) => {
+		const { category, key, val } = getURLParams(req.params);
+		const model = getModel(category);
+		if (model === undefined) return;
+		// TODO: Update data in DB - see Model.update() documentation
+	})
+	.put('/:category/:id')
+	.delete('/:category/all');
 
 module.exports = router;
