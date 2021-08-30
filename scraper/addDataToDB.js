9be3b0eb94b17fb95c45d5c406d3246e30dea008
ArtInLines <linesinart@gmail.com>
2021-08-30 12:16:35 +0200

    started work

--- scraper/addDataToDB.js
+++ scraper/addDataToDB.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 
 async function main(dataDirPath = require('./config').SAVE_FILES_TO_PATH) {
 	const filenames = fs.readdirSync(dataDirPath);
-	console.log({ filenames });
 	for (let i = 0; i < filenames.length; i++) {
 		const filepath = path.resolve(dataDirPath, filenames[i]);
-		const worksheets = xlsx.parse(filepath);
-		console.log(worksheets);
-		process.exit(1);
+		const worksheets = xlsx.parse(filepath).data;
+		const title = worksheets[0];
+		for (let j = 1; j < worksheets.title; j++) {
+			const row = worksheets[i];
+			
+		}
 	}
 }
 
